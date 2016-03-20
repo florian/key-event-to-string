e8bc849ca9e9c390e85069e4e6cb2709dfd36b57
Florian Hartmann <jscoderr@gmail.com>
2016-03-20 12:31:04 +0100

    Add details

--- index.js
+++ index.js
@@ -63,10 +63,27 @@ function buildKeyArray (e) {
 }
 
 function event2string (e) {
-  return buildKeyArray(e.join(options.joinWith)
+  return buildKeyArray(e).join(options.joinWith)
+}
+
+function details (e) {
+	var map = buildKeyMap(e)
+	var mods = map.modifiers
+
+	var hasModifier = mods.cmd || mods.ctrl || mods.alt || mods.shift
+
+	var result = {
+		hasKey: map.character != null,
+		hasModifier: hasModifier,
+		map: map
+	}
+
+	return result
 }
 
 module.exports = function (userOptions) {
 	options = Object.assign(defaultOptions, userOptions)
 	return event2string
 }
+
+module.exports.details = details
